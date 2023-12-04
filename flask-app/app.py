942bef2dc87b81f1f325fad2e01c5f03060212bd
Vincent Liao <vincent@neynar.com>
2023-12-04 08:59:18 +0000

    add: python flask app example

---
+++ flask-app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import requests
+import os
+import dotenv
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+
+@app.route("/")
+def main_app():
+    # Get data from Neynar API
+    dotenv.load_dotenv()
+    api_key = os.getenv("NEYNAR_API_KEY")
+    evm_channel = {
+        "name": "EVM",
+        "parent_url": "chain://eip155:1/erc721:0x37fb80ef28008704288087831464058a4a3940ae",
+        "image": "https://warpcast.com/~/channel-images/evm.png",
+        "channel_id": "evm",
+        "lead_fid": 3621,
+    }
+
+    url = "https://api.neynar.com/v2/"
+    url += "farcaster/feed?feed_type=filter&filter_type=parent_url"
+    url += f"&parent_url={evm_channel['parent_url']}&limit=25"
+    headers = {"accept": "application/json", "api_key": api_key}
+
+    # get data from Neynar API, parse, then render template
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # parse data
+    parser = lambda cast: {"fid": cast["author"]["fid"], "text": cast["text"]}
+    parsed_data = list(map(parser, data["casts"]))
+    parsed_data = [cast for cast in parsed_data if cast["text"] not in ["", None]]
+
+    return render_template("index.html", data=parsed_data)
+
+
+# Run the application
+if __name__ == "__main__":
+    app.run(debug=True)
