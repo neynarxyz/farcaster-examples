7f17ab7173b1d42f0d52931938b2eecc2067b6cf
Shreyaschorge <shreyas@neynar.com>
2023-12-08 19:38:00 +0530

    Add useLocalStorageState hook

--- simple-caster/src/utils/helpers.ts
+++ simple-caster/src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from "react";
+
 export const welcomeMessages = [
   "Join the conversation. Sign in to share your story on Warpcast.",
   "Ready to make your mark? Sign in to start casting on Warpcast.",
@@ -13,3 +15,49 @@ export const welcomeMessages = [
 export const getMessage = (messagesList: string[]) => {
   return messagesList[Math.floor(Math.random() * messagesList.length)];
 };
+
+type DeserializeFunction<T> = (value: string) => T;
+type SerializeFunction<T> = (value: T) => string;
+
+interface UseLocalStorageStateOptions<T> {
+  serialize?: SerializeFunction<T>;
+  deserialize?: DeserializeFunction<T>;
+}
+
+function useLocalStorageState<T>(
+  key: string,
+  defaultValue: T | (() => T) = "" as T,
+  {
+    serialize = JSON.stringify,
+    deserialize = JSON.parse,
+  }: UseLocalStorageStateOptions<T> = {}
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = useState<T>(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      try {
+        return deserialize(valueInLocalStorage);
+      } catch (error) {
+        window.localStorage.removeItem(key);
+      }
+    }
+    return typeof defaultValue === "function"
+      ? (defaultValue as () => T)()
+      : (defaultValue as T);
+  });
+
+  const prevKeyRef = useRef<string>(key);
+
+  useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey);
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, state, serialize]);
+
+  return [state, setState];
+}
+
+export default useLocalStorageState;
