c8ff8aa80b292714e84a684e02437e9b0d8b28cd
Rishav Mukherji <rishavmukherji@gmail.com>
2023-12-13 11:38:59 -0800

    changing tagline text to wowow farcaster

--- wownar/src/utils/helpers.ts
+++ wownar/src/utils/helpers.ts
@@ -3,15 +3,16 @@ import axios, { AxiosError } from "axios";
 import { ErrorRes } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 
 export const welcomeMessages = [
-  "Join the conversation. Sign in to share your story on Warpcast.",
-  "Ready to make your mark? Sign in to start casting on Warpcast.",
-  "Sign in to cast your thoughts and connect with the Warpcast community.",
-  "Be part of the decentralized dialogue. Sign in to cast your first post now.",
-  "Let's get your ideas out there. Sign in to start casting your unique perspective.",
-  "Elevate your voice. Sign in and amplify your message.",
-  "Connect, engage, and influence. Sign in to begin your Warpcast journey.",
-  "Make waves with your words. Sign in and cast away!",
-  "Sign in and join a new era of social networking.",
+  "Wowow Farcaster",
+  // "Join the conversation. Sign in to share your story on Warpcast.",
+  // "Ready to make your mark? Sign in to start casting on Warpcast.",
+  // "Sign in to cast your thoughts and connect with the Warpcast community.",
+  // "Be part of the decentralized dialogue. Sign in to cast your first post now.",
+  // "Let's get your ideas out there. Sign in to start casting your unique perspective.",
+  // "Elevate your voice. Sign in and amplify your message.",
+  // "Connect, engage, and influence. Sign in to begin your Warpcast journey.",
+  // "Make waves with your words. Sign in and cast away!",
+  // "Sign in and join a new era of social networking.",
 ];
 
 export const getMessage = (messagesList: string[]) => {
