f6afac1f8cf7eca4364d414a8784a8bd74f95a1d
Shreyaschorge <shreyas@neynar.com>
2024-01-09 22:53:21 +0530

    Cleanup

--- wownar-react-native/src/components/Screens/Signin.tsx
+++ wownar-react-native/src/components/Screens/Signin.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import { NeynarSigninButton } from "neynar-test";
 
 const Signin = () => {
@@ -8,7 +8,7 @@ const Signin = () => {
 
   return (
     <>
-      <Text style={styles.title}>Wowow Farcaster...</Text>
+      <Text style={styles.title}>Wowow Farcaster</Text>
       <NeynarSigninButton onPress={handleSignIn} />
     </>
   );
@@ -21,20 +21,6 @@ const styles = StyleSheet.create({
     fontSize: 36,
     color: "#fff",
     marginBottom: 24,
-    fontWeight: "200",
+    fontWeight: "100",
   },
-  signInButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 20,
-    marginBottom: 24,
-    minWidth: 218,
-    width: 218,
-  },
-  logo: { width: 24, height: 24, marginRight: 10 },
-  signInText: { fontSize: 16, fontWeight: "300" },
 });
