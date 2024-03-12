4b5e3e28288ac1f22b6a6be37b62eaa93271c3ed
Shreyaschorge <shreyas@neynar.com>
2024-03-12 21:25:47 +0530

    Remove NEXT_PUBLIC_NEYNAR_LOGIN_URL in wownar

--- wownar/src/app/Screens/Signin/index.tsx
+++ wownar/src/app/Screens/Signin/index.tsx
@@ -76,11 +76,8 @@ const Signin = () => {
   const { setSignerUuid, setFid } = useApp();
   const client_id = process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID;
   const redirect_uri = process.env.NEXT_PUBLIC_NEYNAR_REDIRECT_URI;
-  const neynar_login_url = process.env.NEXT_PUBLIC_NEYNAR_LOGIN_URL;
+  const neynar_login_url = "https://app.neynar.com/login";
 
-  if (!neynar_login_url) {
-    throw new Error("NEXT_PUBLIC_NEYNAR_LOGIN_URL is not defined in .env");
-  }
   if (!client_id) {
     throw new Error("NEXT_PUBLIC_NEYNAR_CLIENT_ID is not defined in .env");
   }
