0214b63a28cb295c1a77a2d9debf13b231060fe2
Manan <manan@neynar.com>
2024-03-17 05:41:09 -0700

    chore: move port, update default

--- wownar/src/app/Screens/Signin/index.tsx
+++ wownar/src/app/Screens/Signin/index.tsx
@@ -75,7 +75,7 @@ const Signin = () => {
 
   const { setSignerUuid, setFid } = useApp();
   const client_id = process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID;
-  const neynar_login_url = process.env.NEXT_PUBLIC_NEYNAR_LOGIN_URL || "https://api.neynar.com";
+  const neynar_login_url = process.env.NEXT_PUBLIC_NEYNAR_LOGIN_URL || "https://app.neynar.com/login";
 
   if (!client_id) {
     throw new Error("NEXT_PUBLIC_NEYNAR_CLIENT_ID is not defined in .env");
