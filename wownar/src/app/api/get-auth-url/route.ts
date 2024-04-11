64557a5137d56797303d54faba3f1040fe2c35e5
Shreyaschorge <shreyas@neynar.com>
2024-04-11 13:07:46 +0530

    Add endpoint to get Authorized URL

---
+++ wownar/src/app/api/get-auth-url/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = await fetch(
+      `https://api.neynar.com/v2/farcaster/login/authorize?api_key=${process.env
+        .NEYNAR_API_KEY!}&response_type=code&client_id=${
+        process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID
+      }`
+    );
+
+    if (!response.ok)
+      throw new Error("Something went wrong fetching the authorization URL");
+
+    const { authorization_url } = await response.json();
+
+    return NextResponse.json({ authorization_url }, { status: 200 });
+  } catch (err) {
+    console.log("/api/get-auth-url", err);
+    return NextResponse.json({ message: err.message }, { status: 500 });
+  }
+}
