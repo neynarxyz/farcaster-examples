7c805751b39c2e5120f62d05829ec74109412105
Shreyaschorge <shreyas@neynar.com>
2024-05-10 20:51:08 +0530

    Remove unnecessary vars

--- wownar-react-sdk/src/app/layout.tsx
+++ wownar-react-sdk/src/app/layout.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { AppProvider } from "@/Context/AppContext";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import { NeynarContextProvider, Theme } from "@neynar/react";
 import "@neynar/react/dist/style.css";
 import NeynarProviderWrapper from "@/Context/NeynarProviderWrapper";
 
