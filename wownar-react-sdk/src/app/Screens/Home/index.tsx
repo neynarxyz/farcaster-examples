dbd5c210cd705229e41e83b3aacb92e573069f77
dylsteck <dylancsteck@gmail.com>
2024-08-15 14:49:54 -0400

    wip: pull my branch from `@neynar/react` and  render a frame on `<Home />` in `wownar-react-sdk`

--- wownar-react-sdk/src/app/Screens/Home/index.tsx
+++ wownar-react-sdk/src/app/Screens/Home/index.tsx
@@ -6,11 +6,39 @@ import axios, { AxiosError } from "axios";
 import { toast } from "react-toastify";
 import { ErrorRes } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 import { useState } from "react";
-import { NeynarProfileCard, useNeynarContext } from "@neynar/react";
+import { NeynarFrameCard, NeynarProfileCard, useNeynarContext } from "@neynar/react";
+
+export enum ToastType {
+  Success = "success",
+  Error = "error",
+  Warning = "warning",
+  Info = "info",
+}
+
+const NEYNAR_API_URL = "https://sdk-api.neynar.com";
+
+export type NeynarFrame = {
+  version: string;
+  title: string;
+  image: string;
+  buttons: {
+    index: number;
+    title: string;
+    action_type: string;
+    target?: string;
+    post_url?: string;
+  }[];
+  input: {
+    text?: string;
+  };
+  state: object;
+  frames_url: string;
+};
 
 const Home = () => {
-  const { user } = useNeynarContext();
+  const { user, client_id, showToast } = useNeynarContext();
   const [text, setText] = useState("");
+  const [signerValue, setSignerValue] = useState<string | null>(user?.signer_uuid || null);
 
   async function handlePublishCast() {
     try {
@@ -40,33 +68,73 @@ const Home = () => {
     }
   }
 
+  const handleFrameBtnPress = async (
+    btnIndex: number,
+    localFrame: NeynarFrame,
+    setLocalFrame: React.Dispatch<React.SetStateAction<NeynarFrame>>,
+    inputValue?: string
+  ): Promise<NeynarFrame> => {
+    if (!signerValue) {
+      showToast(ToastType.Error, "Signer UUID is not available");
+      throw new Error("Signer UUID is not available");
+    }
+
+    const button = localFrame.buttons.find((btn: { index: number }) => btn.index === btnIndex);
+    const postUrl = button?.post_url;
+
+    try {
+      const response = await fetchWithTimeout(`${NEYNAR_API_URL}/v2/farcaster/frame/action?client_id=${client_id}`, {
+        method: "POST",
+        headers: {
+          "accept": "application/json",
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          "signer_uuid": signerValue,
+          "action": {
+            "button": button,
+            "frames_url": localFrame.frames_url,
+            "post_url": postUrl ? postUrl : localFrame.frames_url,
+            "input": {
+              "text": inputValue
+            }
+          }
+        })
+      }) as Response;
+
+      if (response.ok) {
+        const json = await response.json() as NeynarFrame;
+        if(Object.keys(json).includes('transaction_calldata')){
+
+        } else{
+          return json;
+        }
+      } else {
+        showToast(ToastType.Error, `HTTP error! status: ${response.status}`);
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch (error) {
+      showToast(ToastType.Error, `An error occurred while processing the button press: ${(error as Error).message}`);
+      throw error;
+    }
+  };
+
   return (
     <ScreenLayout>
       <main className="flex flex-col flex-grow justify-center items-center">
         {user ? (
           <>
             <NeynarProfileCard fid={user.fid} viewerFid={3} />
-            {/* <p className="text-3xl">
-              Hello <span className="font-medium">{user.display_name}</span>
-              ... 👋
-            </p>
-            <div className={styles.inputContainer}>
-              <Image
-                src={user.pfp_url || ""}
-                width={40}
-                height={40}
-                alt="User Profile Picture"
-                className={`${styles.profilePic} rounded-full`}
+            <div className="max-w-[60%] md:max-w-[45%] mt-[2.5%] flex flex-row gap-8 items-end overflow-x-scroll">
+              <NeynarFrameCard 
+                url="https://highlight.xyz/mint/667dfcfe5229c603647108f0" 
+                onFrameBtnPress={handleFrameBtnPress} 
               />
-              <textarea
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                className={styles.userInput}
-                placeholder="Say Something"
-                rows={5}
+              <NeynarFrameCard 
+                url="https://events.xyz/events/a010d617" 
+                onFrameBtnPress={handleFrameBtnPress} 
               />
             </div>
-            <Button onClick={handlePublishCast} title="Cast" /> */}
           </>
         ) : (
           <p>Loading...</p>
@@ -75,5 +143,14 @@ const Home = () => {
     </ScreenLayout>
   );
 };
+
+function fetchWithTimeout(url: string, options: RequestInit, timeout: number = 8000): Promise<Response> {
+  return Promise.race([
+    fetch(url, options),
+    new Promise<Response>((_, reject) =>
+      setTimeout(() => reject(new Error("Request timed out")), timeout)
+    )
+  ]);
+}
 
 export default Home;
