449d1bf64e98ec6ac067ded9be5716cc78f3d805
Shreyaschorge <shreyas@neynar.com>
2024-10-16 18:12:37 +0530

    Remove user verification

--- wownar/src/utils/helpers.ts
+++ wownar/src/utils/helpers.ts
@@ -18,27 +18,3 @@ export const welcomeMessages = [
 export const getMessage = (messagesList: string[]) => {
   return messagesList[Math.floor(Math.random() * messagesList.length)];
 };
-
-export const verifyUser = async (signerUuid: string, fid: string) => {
-  let _isVerifiedUser = false;
-  try {
-    const {
-      data: { isVerifiedUser },
-    } = await axios.post("/api/verify-user", { signerUuid, fid });
-    _isVerifiedUser = isVerifiedUser;
-  } catch (err) {
-    const { message } = (err as AxiosError).response?.data as ErrorRes;
-    toast(message, {
-      type: "error",
-      theme: "dark",
-      autoClose: 3000,
-      position: "bottom-right",
-      pauseOnHover: true,
-    });
-  }
-  return _isVerifiedUser;
-};
-
-export const removeSearchParams = () => {
-  window.history.replaceState({}, document.title, window.location.pathname);
-};
